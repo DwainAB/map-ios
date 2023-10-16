467b34fc14bba6b59b33ff880e45ee71b923d009
Dwain <dwaincontact@gmail.com>
2023-10-16 16:22:45 +0200

    Initial Commit

---
+++ testNAV/testNAVApp.swift
@@ -0,0 +1,17 @@
+//
+//  testNAVApp.swift
+//  testNAV
+//
+//  Created by Dwain Abrivard on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct testNAVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
